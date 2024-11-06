c083f08e1f424d16db3267259662c443594f83ca
Daavipf <davi.pferreira2705@gmail.com>
2024-11-06 15:11:24 -0300

    Primeiro commit

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -22,4 +22,52 @@ export default class UserController {
         .json({ message: "Houve um erro ao buscar todos os usuários" });
     }
   }
+
+  static async getUserById(req: Request, res: Response) {
+    const { userId } = req.params;
+    try {
+      const user = await User.findOne({ where: { id: userId } });
+      return res.status(200).json(user);
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: "Houve um erro ao buscar um usuário" });
+    }
+  }
+
+  static async updateUser(req: Request, res: Response) {
+    const { userId } = req.params;
+    const { name, email, password } = req.body;
+    try {
+      const [updatedRows, updatedUsers] = await User.update(
+        { name, email, password },
+        { where: { id: userId }, returning: true }
+      );
+      if (updatedRows === 0) {
+        return res.status(404).json({ message: "Usuário não encontrado" });
+      }
+      return res
+        .status(201)
+        .json({ message: "Usuário atualizado", user: updatedUsers[0] });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: "Houve um erro ao editar um usuário" });
+    }
+  }
+
+  static async deleteUser(req: Request, res: Response) {
+    const { userId } = req.params;
+    try {
+      const user = await User.destroy({ where: { id: userId } });
+      if (!user) {
+        return res.status(404).json({ message: "Usuário não encontrado" });
+      }
+      return res.status(201).json({ message: "Usuário apagado" });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: "Houve um erro ao deletar um usuário" });
+    }
+  }
 }
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -9,5 +9,14 @@ router.post("/create", (req: Request, res: Response) => {
 router.get("/get-all", (req: Request, res: Response) => {
   UserController.getUsers(req, res);
 });
+router.get("/get/:userId", (req: Request, res: Response) => {
+  UserController.getUserById(req, res);
+});
+router.put("/update/:userId", (req: Request, res: Response) => {
+  UserController.updateUser(req, res);
+});
+router.delete("/delete/:userId", (req: Request, res: Response) => {
+  UserController.deleteUser(req, res);
+});
 
 export default router;
